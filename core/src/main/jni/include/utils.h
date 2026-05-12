0d1b5dbdcfffb5e464ea5ad99afceb54db953837
dd <you@example.com>
2026-05-12 06:42:36 +0000

    fix: read Android API level from libc properties

--- core/src/main/jni/include/utils.h
+++ core/src/main/jni/include/utils.h
@@ -26,6 +26,7 @@
 #include <string>
 #include <cstdlib>
 #include <cstring>
+#include <dlfcn.h>
 #include <filesystem>
 #include <api/system_properties.h>
 #include <unistd.h>
@@ -35,20 +36,41 @@
 namespace lspd {
     using namespace std::literals::string_literals;
 
+    inline int GetSystemProperty(const char *name, char *value) {
+        using SystemPropertyGet = int (*)(const char *, char *);
+        static auto system_property_get = []() -> SystemPropertyGet {
+            if (auto libc = dlopen("libc.so", RTLD_NOW | RTLD_LOCAL)) {
+                return reinterpret_cast<SystemPropertyGet>(dlsym(libc, "__system_property_get"));
+            }
+            return nullptr;
+        }();
+        if (system_property_get) {
+            if (auto len = system_property_get(name, value); len > 0) {
+                return len;
+            }
+        }
+        static auto properties_initialized = __system_properties_init() == 0;
+        if (properties_initialized) {
+            return __system_property_get(name, value);
+        }
+        value[0] = '\0';
+        return 0;
+    }
+
     inline int32_t GetAndroidApiLevel() {
         static int32_t api_level = []() {
             char prop_value[PROP_VALUE_MAX]{};
             int base = 0;
-            if (__system_property_get("ro.build.version.sdk", prop_value) > 0) {
+            if (GetSystemProperty("ro.build.version.sdk", prop_value) > 0) {
                 base = atoi(prop_value);
             }
             int preview = 0;
-            if (__system_property_get("ro.build.version.preview_sdk", prop_value) > 0) {
+            if (GetSystemProperty("ro.build.version.preview_sdk", prop_value) > 0) {
                 preview = atoi(prop_value);
             }
             char codename[PROP_VALUE_MAX]{};
             if (preview > 0 &&
-                __system_property_get("ro.build.version.codename", codename) > 0 &&
+                GetSystemProperty("ro.build.version.codename", codename) > 0 &&
                 strcmp(codename, "REL") != 0) {
                 return base + preview;
             }
